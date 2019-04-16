6223db077fe801a6dd1bcd4c56ffaae20633c537
Keith <ozmaneku@gmail.com>
2019-04-16 11:00:26 -0400

    Fixed a typo

--- Final_Fantasy_Project/settings.py
+++ Final_Fantasy_Project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_extenstions',
+    'django_extensions',
     'Final_Fantasy_App',
 ]
 
