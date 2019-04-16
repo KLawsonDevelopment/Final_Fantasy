8fe0f34703e1bb542957e86ad134689add213ad4
Keith <ozmaneku@gmail.com>
2019-04-16 12:03:09 -0400

    Added python formatter

--- Final_Fantasy_App/models.py
+++ Final_Fantasy_App/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Character(models.model):
+    Name = models.CharField(max_length=255)
+    Portrait = models.CharField(max_length=500)
+    Avatar = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Gear_Details(models.model):
+    Body = models.CharField(max_length=255)
+    Bracelets = models.CharField(max_length=255)
+    Earrings = models.CharField(max_length=255)
+    Feet = models.CharField(max_length=255)
+    Hands = models.CharField(max_length=255)
+    Head = models.CharField(max_length=255)
+    Legs = models.CharField(max_length=255)
+    MainHand = models.CharField(max_length=255)
+    Necklace = models.CharField(max_length=255)
+    Ring1 = models.CharField(max_length=255)
+    Ring2 = models.CharField(max_length=255)
+    SoulCrystal = models.CharField(max_length=255)
+    Waist = models.CharField(max_length=255)
