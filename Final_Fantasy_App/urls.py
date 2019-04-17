1a13349c71c378728bc81585f4dd1a9c97412b08
Keith <ozmaneku@gmail.com>
2019-04-17 10:46:16 -0400

    Finished views and urls in app:

--- Final_Fantasy_App/urls.py
+++ Final_Fantasy_App/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('characters', views.CharacterView)
+router.register('details', views.DetailView)
+router.register('pieces', views.PieceView)
+router.register('wanted', views.WantedView)
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- Final_Fantasy_App/views.py
+++ Final_Fantasy_App/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CharacterSerializer, PieceSerializer, DetailSerializer, WantedSerializer
+from .models import Character, Gear_Details, GearPiece, WantedGear
 
 # Create your views here.
+
+class CharacterView(viewsets.ModelViewSet):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+class DetailView(viewsets.ModelViewSet):
+    queryset = Gear_Details.objects.all()
+    serializer_class = DetailSerializer
+
+class PieceView(viewsets.ModelViewSet):
+    queryset = GearPiece.objects.all()
+    serializer_class = PieceSerializer
+
+class WantedView(viewsets.ModelViewSet):
+    queryset = WantedGear.objects.all()
+    serializer_class = WantedSerializer
