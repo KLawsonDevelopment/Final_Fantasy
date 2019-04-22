e2828ba29b1ae176ab66980b9b23919dc934934f
Keith <ozmaneku@gmail.com>
2019-04-22 09:47:30 -0400

    Added in catch all route

--- client/src/components/CharacterGear.js
+++ client/src/components/CharacterGear.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import Axios from 'axios';
 
-//Forcing a change for heroku
-
 class CharacterGear extends Component {
     state = {
         character: {
