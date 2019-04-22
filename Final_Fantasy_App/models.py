37caa36d66199ff1c0685db90caf3fd0be4b5afa
Keith <ozmaneku@gmail.com>
2019-04-22 14:58:34 -0400

    Removing comments

--- Final_Fantasy_App/models.py
+++ Final_Fantasy_App/models.py
@@ -14,48 +14,6 @@ class Character(models.Model):
         return self.Name
 
 
-# class Gear_Details(models.Model):
-#     Name = models.CharField(max_length=255, default="name")
-#     Body = models.CharField(max_length=255, default="body")
-#     Bracelets = models.CharField(max_length=255, default="Bracelets")
-#     Earrings = models.CharField(max_length=255, default="Earrings")
-#     Feet = models.CharField(max_length=255, default="Feet")
-#     Hands = models.CharField(max_length=255, default="Hands")
-#     Head = models.CharField(max_length=255, default="Head")
-#     Legs = models.CharField(max_length=255, default="Legs")
-#     MainHand = models.CharField(max_length=255, default="MainHand")
-#     Necklace = models.CharField(max_length=255, default="Necklace")
-#     Ring1 = models.CharField(max_length=255, default="Ring1")
-#     Ring2 = models.CharField(max_length=255, default="Ring2")
-#     SoulCrystal = models.CharField(max_length=255, default="SoulCrystal")
-#     Waist = models.CharField(max_length=255, default="Waist")
-#     Character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='gear_details')
-
-#     def __get_tuple(self):
-#         return (
-#             (self.Body,
-#             self.Bracelets,
-#             self.Earrings,
-#             self.Feet,
-#             self.Hands,
-#             self.Legs,
-#             self.MainHand,
-#             self.Necklace,
-#             self.Ring1,
-#             self.Ring2,
-#             self.SoulCrystal,
-#             self.Waist),
-#         )
-    
-#     def __getitem__(self):
-#         return self.__get_tuple()[key]
-
-#     def __iter__(self):
-#         return iter(self.__get_tuple())
-
-#     def __str__(self):
-#         return self.Name
-
 class GearPiece(models.Model):
     GEAR_TYPE_CHOICES = (
         ('Body', 'Body'),
